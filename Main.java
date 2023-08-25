1459fb8de8d9ccc190b7d4538b77120a6602f3a4
pawelarol <123151436+pawelarol@users.noreply.github.com>
2023-08-25 21:26:36 +0200

    Deleted file

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.display();
-    }
-}
