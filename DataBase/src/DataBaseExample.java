e1b94cf93fd946499c2914b862e500b844fd2d9f
pawelarol <123151436+pawelarol@users.noreply.github.com>
2023-09-10 19:43:29 +0200

    Added "Close methods" for Statement and ResultSet.

--- DataBase/src/DataBaseExample.java
+++ DataBase/src/DataBaseExample.java
@@ -4,31 +4,40 @@ import java.util.List;
 
 public class DataBaseExample {
     public static void main(String[] args) throws Exception {
-
+        List<Person> result = new ArrayList<>();
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/Example?currentSchema=public";
 
         // org.postgresql.jdbc.PgConnection c;
-
         Connection con = DriverManager.getConnection(url, "postgres", "pawel2510");
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT id_contact, name_contact, gender, city FROM first");
-        List<Person> result = new ArrayList<>();
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM first");
 
-        while (rs.next()) {
-            Long id = rs.getLong("id_contact");
-            String name = rs.getString("name_contact");
-            String gender = rs.getString("gender");
-            String city = rs.getString("city");
+            while (rs.next()) {
+                Long id = rs.getLong("id_contact");
+                String name = rs.getString("name_contact");
+                String gender = rs.getString("gender");
+                String city = rs.getString("city");
 
-            Person person = new Person(id,name,gender,city);
-            result.add(person);
+                Person person = new Person(id, name, gender, city);
+                result.add(person);
+                for(Person p : result){
+                    System.out.println(p);
+                }
+            }
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            con.close();
         }
-
-        for (Person p : result) {
-            System.out.println(p);
         }
-
-
     }
-}
+
